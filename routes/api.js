b9eb70d4a5f145ae8905451758c88f25c8857d55
bdjm <brendandjmoore@gmail.com>
2021-07-05 22:16:06 +1000

    updating the put route

--- routes/api.js
+++ routes/api.js
@@ -27,13 +27,19 @@ router.post("/api/workouts", ({ body }, res) => {
         });
 });
 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-    .then((dbWorkout) => {
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate({
+        _id: req.params.id 
+    }, {
+        $push: { exercises: req.body }
+    }, {
+        new: true
+    })
+    .then(dbWorkout => {
         res.json(dbWorkout);
     })
-    .catch((err) => {
-        res.json(err);
+    .catch(err => {
+        res.status(400).json(err);
     });
 });
 
