4b83f557ec42f46f19f1ed2d54bec4a7f57b5634
bdjm <brendandjmoore@gmail.com>
2021-07-05 16:34:09 +1000

    adding the route to update a workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-router.post("/api/workouts", async ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
     .then(dbWorkout => {
         res.json(dbWorkout);
@@ -9,4 +9,15 @@ router.post("/api/workouts", async ({ body }, res) => {
     .catch(err => {
         res.status(400).json(err);
     });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id,
+        { $push: { exercises: body } })
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
