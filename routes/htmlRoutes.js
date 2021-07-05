7a361c81bbf87610a04905e49f7d5b937ca4a23f
bdjm <brendandjmoore@gmail.com>
2021-07-05 15:23:01 +1000

    adding the exercise route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,3 +4,7 @@ var path = require("path");
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
