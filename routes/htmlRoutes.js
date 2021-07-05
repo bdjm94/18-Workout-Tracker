b534527116f6e69a2d3bbfa05eb75d5c34b30aeb
bdjm <brendandjmoore@gmail.com>
2021-07-05 15:23:42 +1000

    adding the stats page route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,3 +8,9 @@ router.get("/", (req, res) => {
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
