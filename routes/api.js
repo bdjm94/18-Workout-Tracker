c17ba61ef4c63301bb3c55bee1072f557bba8896
bdjm <brendandjmoore@gmail.com>
2021-07-05 22:12:03 +1000

    updating the post routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-router.post("/api/workouts", ({ body }, res) => {
+router.get("/api/workouts", ({ body }, res) => {
     Workout.aggregate([{
         $addFields: {
             totalDuration: {
@@ -17,14 +17,13 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(params.id,
-        { $push: { exercises: body } })
-        .then((dbWorkout) => {
-            res.json(dbWorkout);
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
         })
         .catch((err) => {
-            res.json(err);
+            res.status(400).json(err);
         });
 });
 
