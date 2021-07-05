16091f25e579574bd88dd112976476699924aefe
bdjm <brendandjmoore@gmail.com>
2021-07-05 14:50:20 +1000

    updating the server file and adding the routes to the api and html files

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
