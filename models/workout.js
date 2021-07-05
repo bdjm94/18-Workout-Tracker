bdd84f48223ab82dfd9e1654627c354a6e4d1081
bdjm <brendandjmoore@gmail.com>
2021-07-05 17:15:18 +1000

    adding the exercises to the workout schema

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+day: {
+    type: Date,
+    default: Date.now
+},
+exercises: [{
+    type: {
+        type: String,
+        trim: true,
+        required: "What workout are you doing today?"
+    },
+}]
+})
