e756d11853080b4be978e4df5a26c754bf727605
bdjm <brendandjmoore@gmail.com>
2021-07-05 16:28:36 +1000

    adding the route to create a new workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
+
+router.post("/api/workouts", async ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
